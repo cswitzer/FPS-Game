a772fcb5383cc5094b2ff84be540594ea34f6b4a
cswitzer <50593813+cswitzer@users.noreply.github.com>
2021-02-01 20:04:40 -0700

    Added a gun
    Camera shoots out raycast and returns a hit

---
+++ FPS-Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] float damage = 10f;
+    [SerializeField] float range = 100f;
+
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+        HandleGunVerticalRotation();
+    }
+
+    private static void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // place shooting code using raycasting (using the camera)
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // does the actual raycasting and returns true if we hit something
+            if (Physics.Raycast(ray, out hit, 100));
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+
+    void HandleGunVerticalRotation()
+    {
+
+    }
+
+}
--- FPS-Game/Assets/Scripts/PlayerMovement.cs
+++ FPS-Game/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] CharacterController controller;
     [SerializeField] float speed = 20f;
     [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpHeight = 3f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
 
     Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +25,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckIfGrounded();
         ControlPlayerMovement();
-        HandlePlayerFalling();
+        HandleJumping();
+        HandleGravity();
+    }
+
+    private void CheckIfGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance /*radius*/, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            // lower than zero so we can force our player down on the ground
+            velocity.y = -2f;
+        }
     }
 
     private void ControlPlayerMovement()
@@ -33,7 +52,15 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
     }
 
-    private void HandlePlayerFalling()
+    private void HandleJumping()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+
+    private void HandleGravity()
     {
         // formula for falling in 3d is (1/2)g * t^2, so Time.deltaTime needs to multiply twice
         velocity.y += gravity * Time.deltaTime;
